d130458add83b7a8c290ea3984259c24b4be323e
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-13 19:37:34 +0530

    6 files modified, 2 added

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,8 +2,16 @@ import React from 'react';
 export default function Avatar() {
   return (
     <div>
-      <img src="" width="50" height="50" />
-      Avatar
+      <div class="container">
+        <div class="d-flex justify-content-center h-100">
+          <div class="image_outer_container">
+            <div class="green_icon"></div>
+            <div class="image_inner_container">
+              <img src="https://i.pinimg.com/originals/43/96/61/439661dcc0d410d476d6d421b1812540.jpg" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
