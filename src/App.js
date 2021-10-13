2d39343196b35a86335c405ed5606ecfebc899ac
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-13 17:52:24 +0530

    1 file modified, 7 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './style.css';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Stories from './Stories';
+import Content from './components/Content';
+import Post from './components/Post';
+export default function Content() {
+  return (
+    <div>
+      Content
+      <Stories />
+      <Post />
+    </div>
+  );
+}
