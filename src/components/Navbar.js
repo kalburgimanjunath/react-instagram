b4e524f9e6389b959844a84cd7a26310b72c05bc
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-13 18:43:21 +0530

    2 files modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,15 @@
 import React from 'react';
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      Navbar
+      <ul>
+        <li>Home</li>
+        <li>Chat</li>
+        <li>Explore</li>
+        <li>Notification</li>
+        <li>Profile</li>
+      </ul>
+    </div>
+  );
 }
