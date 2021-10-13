52cf30673feb2a3d4df536b9013f76ad3c58136e
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-13 21:02:32 +0530

    4 files modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -122,23 +122,6 @@ export default function Navbar() {
           </div>
         </div>
       </nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/chat">Chat</Link>
-        </li>
-        <li>
-          <Link to="/explore">Explore</Link>
-        </li>
-        <li>
-          <Link to="/notification">Notification</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-      </ul>
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Avatar from './Avatar';
 import PostItem from './PostItem';
-export default function Post() {
-  const userdata = [
+export default class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [
+        {
+          firstname: 'manjunath',
+          lastname: 'kalburgi',
+          location: 'Ravi nagar',
+          city: 'hubli',
+          country: 'india',
+          post_image_url: 'https://via.placeholder.com/300/09f/fff.png',
+          post_video_url: '',
+        },
+        {
+          firstname: 'Suman',
+          lastname: 'Habib',
+          location: 'Ravi nagar',
+          city: 'hubli',
+          country: 'india',
+          post_image_url: 'https://via.placeholder.com/300/09f/fff.png',
+          post_video_url: '',
+        },
+      ],
+    };
+  }
+  userdata = [
     {
       firstname: 'manjunath',
       lastname: 'kalburgi',
@@ -22,13 +49,15 @@ export default function Post() {
       post_video_url: '',
     },
   ];
-  return (
-    <div>
-      Post
-      <Avatar user={userdata} />
-      <div>Full Name</div>
-      <div>Address,Location,City,Country</div>
-      <PostItem user={userdata} />
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        Post
+        <Avatar user={this.userdata} />
+        <div>Full Name</div>
+        <div>Address,Location,City,Country</div>
+        <PostItem user={this.userdata} />
+      </div>
+    );
+  }
 }
